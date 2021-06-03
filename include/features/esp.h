8044d361b9f72f795bce687bdc19b2d39653e60b
ses <sesame03@protonmail.com>
2021-06-03 02:52:40 -0400

    base esp

---
+++ include/features/esp.h
@@ -0,0 +1,8 @@
+#ifndef FEATURES_ESP_H
+#define FEATURES_ESP_H
+
+#include "ses.h"
+
+void features_esp_run( );
+
+#endif // !FEATURES_ESP_H
---
+++ src/features/esp.c
@@ -0,0 +1,6 @@
+#include "features/esp.h"
+#include "gui/config.h"
+
+void features_esp_run() {
+
+}
